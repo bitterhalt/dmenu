41488353c5dff51ba26599492b758114e6792b7b
bitterhalt <95308907+bitterhalt@users.noreply.github.com>
2023-08-28 15:33:58 +0300

    add dmenu number

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bt = 0;               /* add an defined amount of pixels to the bar height */
+static const int user_bt = 5;               /* add an defined amount of pixels to the bar height */
 
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrains Mono:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
